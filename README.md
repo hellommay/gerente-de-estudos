fed7cae8429b7fded8e46570d70b647726a8ab6f
hellommay <hellommay@hotmail.com>
2026-06-19 17:39:12 -0300

    feat: add README e implementa gravacao de sessoes no arquivo sessoes.txt

---
+++ README.md
@@ -0,0 +1,8 @@
+# GERENTE DE ESTUDOS (CLI)
+
+Uma ferramenta de linha de comando em Python criada para gerenciar e registrar sessões de estudos ao longo das fases de aprendizado.
+
+#### Como rodar:
+* Ter Python 3 instalado na máquina
+* Clonar o repositorio e navegar até a pasta
+* Executar o comando `python3 gerente.py`
--- src/gerente.py
+++ src/gerente.py
@@ -11,3 +11,20 @@ def ler_inteiro(mensagem):
         except ValueError:
             print("ERROR: Deve digitar um valor inteiro válido.")
 
+def registrar_sessao():
+    print("\n --- REGISTRAR NOVA VERSÃO ---")
+
+    fase = ler_inteiro("Digite o numero da fase estudada: ")
+    minutos = ler_inteiro("Digite o tempo de estudo (em minutos): ")
+    nota = ler_inteiro("Dê uma nota de 1 a 5 para a sessão: ")
+
+    while nota < 1 or nota > 5:
+        print("ERROR: A nova deve ser de 1 à 5.")
+        nota = ler_inteiro("Dê uma nota de 1 a 5 para a sessão: ")
+
+    data_atual = datetime.now().strftime("%d-%m-%Y %H:%M")
+
+    with open(ARQUIVO_DADOS, "a", encoding="utf-8") as arquivo:
+        arquivo.write(f"{data_atual};{fase};{minutos};{nota}\n")
+
+    print("Sessão registrada com sucesso!")
